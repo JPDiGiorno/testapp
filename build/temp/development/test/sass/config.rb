13519a6226756043b1937a1cec212549ec62d36a
John D'Argenio <jpdarg6@gmail.com>
2018-06-11 20:10:29 -0400

    first commit

---
+++ build/temp/development/test/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = '/Users/johndargenio/Desktop/PETOR_THE_MEXICAN/git_stuff/testapp/build/.sass-cache'
---
+++ build/temp/development/test/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/johndargenio/Desktop/PETOR_THE_MEXICAN/git_stuff/testapp/app.js
